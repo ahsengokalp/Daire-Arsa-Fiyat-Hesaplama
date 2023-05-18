b8bc1d8f7828667d4a989a448d80ffea14e29714
ahsengokalp <128702801+ahsengokalp@users.noreply.github.com>
2023-05-18 16:41:18 +0300

    Update DaireArsaFiyatHesaplama1.py
    iki farklı şekilde arsa olucağından cember ve dikdörtgen şeklindeki arsaların çevresini hesaplayan fonksiyonlar eklendi.

--- DaireArsaFiyatHesaplama1.py
+++ DaireArsaFiyatHesaplama1.py
@@ -1,6 +1,14 @@
+def cember_cevre_hesaplama(yaricap):
+    PI=3.14
+    sonuc=2*PI*yaricap
+    return sonuc
+def dıkdortgen_cevre_hesaplama(kenar1,kenar2):
+    sonuc=2*(kenar1+kenar2)
+    return sonuc
 cikis='h'
 while cikis!='E' and cikis!='e':
     print("1-Daire Fiyati Hesaplama")
     print("2-Arsa Fiyati Hesaplama")
     print("0-cikis")
     sayi=int(input("Seçiminizi giriniz: "))
+   
