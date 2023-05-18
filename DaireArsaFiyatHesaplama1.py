dc887e020346bfc4766e5e5d7dba422d5de15dca
ahsengokalp <128702801+ahsengokalp@users.noreply.github.com>
2023-05-18 16:18:56 +0300

    Add files via upload

---
+++ DaireArsaFiyatHesaplama1.py
@@ -0,0 +1,6 @@
+cikis='h'
+while cikis!='E' and cikis!='e':
+    print("1-Daire Fiyati Hesaplama")
+    print("2-Arsa Fiyati Hesaplama")
+    print("0-cikis")
+    sayi=int(input("Seçiminizi giriniz: "))
